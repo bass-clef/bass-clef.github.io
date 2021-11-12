305222b812d99e074d4a1d7524dfd1efa4918389
Humi@bass_clef_ <bassclef.nico@gmail.com>
2021-11-12 20:16:56 +0900

    Blog bug fix

--- js/wasm.js
+++ js/wasm.js
@@ -722,20 +722,20 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper209 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 8, __wbg_adapter_22);
+    imports.wbg.__wbindgen_closure_wrapper205 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 6, __wbg_adapter_22);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper211 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 10, __wbg_adapter_25);
+    imports.wbg.__wbindgen_closure_wrapper207 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 8, __wbg_adapter_25);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1905 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 427, __wbg_adapter_28);
+    imports.wbg.__wbindgen_closure_wrapper1889 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 424, __wbg_adapter_28);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2204 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 494, __wbg_adapter_31);
+    imports.wbg.__wbindgen_closure_wrapper2188 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 491, __wbg_adapter_31);
         return addHeapObject(ret);
     };
 
